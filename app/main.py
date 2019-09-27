861370fe21dd7b13aa8849c68c03fb73ce92db8b
Denis Kayumov <dencooler@gmail.com>
2019-09-27 23:55:51 +0300

    Initial app setup

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from environs import Env
+from sanic import Sanic
+
+from app.settings import Settings
+
+app = Sanic(__name__)
+
+
+def init():
+    env = Env()
+    env.read_env()
+    app.config.from_object(Settings)
+
+    app.run(
+        host=app.config.HOST,
+        port=app.config.PORT,
+        debug=app.config.DEBUG,
+        auto_reload=app.config.DEBUG,
+    )
