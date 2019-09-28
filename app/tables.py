dc8ea864e2d0527bb811ba20fd961bd4e09a2e2f
Denis Kayumov <dencooler@gmail.com>
2019-09-28 16:19:17 +0300

    setup DB tables and Alembic migrations

---
+++ app/tables.py
@@ -0,0 +1,44 @@
+import sqlalchemy
+
+from datetime import datetime
+from sqlalchemy.orm import relationship
+
+metadata = sqlalchemy.MetaData()
+
+
+comment = sqlalchemy.Table(
+    'comments',
+    metadata,
+    sqlalchemy.Column('id', sqlalchemy.Integer(), primary_key=True, nullable=False),
+    sqlalchemy.Column('text', sqlalchemy.Text()),
+    sqlalchemy.Column('date_created', sqlalchemy.DateTime(), default=datetime.now()),
+)
+
+
+post = sqlalchemy.Table(
+    'posts',
+    metadata,
+    sqlalchemy.Column('id', sqlalchemy.Integer(), primary_key=True, nullable=False),
+    sqlalchemy.Column('theme', sqlalchemy.String(length=100)),
+    sqlalchemy.Column('description', sqlalchemy.String(length=150)),
+    sqlalchemy.Column('date_created', sqlalchemy.DateTime(), default=datetime.now()),
+    sqlalchemy.Column('date_modified', sqlalchemy.DateTime(), default=datetime.now()),
+    sqlalchemy.Column('text', sqlalchemy.Text()),
+    sqlalchemy.Column('comment_id', sqlalchemy.Integer(), sqlalchemy.ForeignKey('comments.id'))
+    #relationship('comments', cascade='all, delete', back_populates='posts'),
+)
+
+
+section = sqlalchemy.Table(
+    'sections',
+    metadata,
+    sqlalchemy.Column('id', sqlalchemy.Integer(), primary_key=True, nullable=False),
+    sqlalchemy.Column('theme', sqlalchemy.String(length=100)),
+    sqlalchemy.Column('description', sqlalchemy.String(length=150)),
+    sqlalchemy.Column('date_created', sqlalchemy.DateTime(), default=datetime.now()),
+    sqlalchemy.Column('date_modified', sqlalchemy.DateTime(), default=datetime.now()),
+    sqlalchemy.Column('post_id', sqlalchemy.Integer(), sqlalchemy.ForeignKey('posts.id'))
+    #relationship('posts', cascade='all, delete', back_populates='sections'),
+)
+
+
